34369d9e93220a0c994b927f140fae3c1f7bbd0a
PangKiYeon <pdrkeamy@gmail.com>
2026-06-25 14:48:00 +0900

    chore: eslint, prettier 설정

---
+++ packages/eslint-config/react-native.js
@@ -0,0 +1,38 @@
+import js from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import tseslint from 'typescript-eslint';
+import pluginReactHooks from 'eslint-plugin-react-hooks';
+import pluginReact from 'eslint-plugin-react';
+import globals from 'globals';
+import { config as baseConfig } from './base.js';
+
+/** @type {import("eslint").Linter.Config[]} */
+export const reactNativeConfig = [
+  ...baseConfig,
+  js.configs.recommended,
+  eslintConfigPrettier,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    languageOptions: {
+      ...pluginReact.configs.flat.recommended.languageOptions,
+      globals: {
+        ...globals.node,
+        __DEV__: 'readonly',
+      },
+    },
+  },
+  {
+    plugins: {
+      'react-hooks': pluginReactHooks,
+    },
+    settings: { react: { version: 'detect' } },
+    rules: {
+      ...pluginReactHooks.configs.recommended.rules,
+      'react/react-in-jsx-scope': 'off',
+    },
+  },
+  {
+    ignores: ['.expo/**', 'dist/**'],
+  },
+];
