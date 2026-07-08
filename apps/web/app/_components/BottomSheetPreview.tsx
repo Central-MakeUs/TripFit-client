8a9654247443349935607b50468ec51559bc6bce
nue-os <kimsoeun0108@gmail.com>
2026-07-09 01:19:15 +0900

    feat: BottomSheetListItem 사용 예시 코드 추가

--- apps/web/app/_components/BottomSheetPreview.tsx
+++ apps/web/app/_components/BottomSheetPreview.tsx
@@ -3,6 +3,7 @@
 import { useState } from 'react';
 
 import BottomSheet from '@/components/bottom-sheet';
+import BottomSheetListItem from '@/components/bottom-sheet-list-item';
 
 function BottomSheetPreview() {
   const [open, setOpen] = useState(false);
@@ -16,11 +17,19 @@ function BottomSheetPreview() {
       >
         바텀시트 열기
       </button>
-      <BottomSheet open={open} onOpenChange={setOpen} title="여행방 초대하기">
-        <p>아래로 드래그하거나 배경을 눌러서 닫을 수 있어요.</p>
-        <p>아래로 드래그하거나 배경을 눌러서 닫을 수 있어요.</p>
-        <p>아래로 드래그하거나 배경을 눌러서 닫을 수 있어요.</p>
-        <p>아래로 드래그하거나 배경을 눌러서 닫을 수 있어요.</p>
+      <BottomSheet open={open} onOpenChange={setOpen} title="여행방 관리">
+        <div className="grid grid-cols-2 gap-2">
+          <BottomSheetListItem selected={false}>
+            default (미선택)
+          </BottomSheetListItem>
+          <BottomSheetListItem selected>default (선택)</BottomSheetListItem>
+          <BottomSheetListItem category="red" selected={false}>
+            Red (미선택)
+          </BottomSheetListItem>
+          <BottomSheetListItem category="red" selected>
+            Red (선택)
+          </BottomSheetListItem>
+        </div>
       </BottomSheet>
     </>
   );
