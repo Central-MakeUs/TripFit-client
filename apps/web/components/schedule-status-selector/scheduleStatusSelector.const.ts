0bc6017e572601e2190cd7528435dc22c2a63e90
nue-os <kimsoeun0108@gmail.com>
2026-07-09 13:47:54 +0900

    refactor: schedule-status 아이콘 import 이름 축약 반영

--- apps/web/components/schedule-status-selector/scheduleStatusSelector.const.ts
+++ apps/web/components/schedule-status-selector/scheduleStatusSelector.const.ts
@@ -1,8 +1,8 @@
 import { FunctionComponent, SVGProps } from 'react';
 
-import AfternoonUnavailableIcon from '@/assets/icons/schedule-status-afternoon-unavailable.svg';
-import EveningUnavailableIcon from '@/assets/icons/schedule-status-evening-unavailable.svg';
-import MorningUnavailableIcon from '@/assets/icons/schedule-status-morning-unavailable.svg';
+import AmUnavailableIcon from '@/assets/icons/schedule-status-am-unavailable.svg';
+import EveUnavailableIcon from '@/assets/icons/schedule-status-eve-unavailable.svg';
+import PmUnavailableIcon from '@/assets/icons/schedule-status-pm-unavailable.svg';
 import UncertainIcon from '@/assets/icons/schedule-status-uncertain.svg';
 import UnavailableIcon from '@/assets/icons/schedule-status-unavailable.svg';
 
@@ -19,20 +19,8 @@ export const SCHEDULE_STATUS_OPTIONS: {
   Icon: FunctionComponent<SVGProps<SVGSVGElement>>;
 }[] = [
   { value: 'unavailable', label: '불가능', Icon: UnavailableIcon },
-  {
-    value: 'morningUnavailable',
-    label: '아침불가',
-    Icon: MorningUnavailableIcon,
-  },
-  {
-    value: 'afternoonUnavailable',
-    label: '점심불가',
-    Icon: AfternoonUnavailableIcon,
-  },
-  {
-    value: 'eveningUnavailable',
-    label: '저녁불가',
-    Icon: EveningUnavailableIcon,
-  },
+  { value: 'morningUnavailable', label: '아침불가', Icon: AmUnavailableIcon },
+  { value: 'afternoonUnavailable', label: '점심불가', Icon: PmUnavailableIcon },
+  { value: 'eveningUnavailable', label: '저녁불가', Icon: EveUnavailableIcon },
   { value: 'uncertain', label: '불확실', Icon: UncertainIcon },
 ];
