7e90fc32b97c7c5fa514f5a84c5804eca5e711f4
nue-os <kimsoeun0108@gmail.com>
2026-07-10 15:51:10 +0900

    fix: type=number Input의 기본 스피너 버튼 제거

--- apps/web/components/input/index.tsx
+++ apps/web/components/input/index.tsx
@@ -75,7 +75,7 @@ function Input({
           onChange={onChange}
           disabled={disabled}
           readOnly={readOnly}
-          className={`text-body-04 text-grey-800 min-w-0 flex-1 outline-none placeholder:text-body-04 placeholder:text-grey-200 ${className ?? ''}`}
+          className={`text-body-04 text-grey-800 min-w-0 flex-1 outline-none placeholder:text-body-04 placeholder:text-grey-200 [-moz-appearance:textfield] [&::-webkit-inner-spin-button]:m-0 [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:m-0 [&::-webkit-outer-spin-button]:appearance-none ${className ?? ''}`}
           {...rest}
         />
         {resolvedSuffixSlot && (
