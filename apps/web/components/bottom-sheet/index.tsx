f9d06aaf0510d4e2e7f599d958144633f822c746
nue-os <kimsoeun0108@gmail.com>
2026-07-10 15:15:46 +0900

    fix: BottomSheet가 데스크톱 프리뷰 프레임 밖으로 넘치는 문제 수정

--- apps/web/components/bottom-sheet/index.tsx
+++ apps/web/components/bottom-sheet/index.tsx
@@ -24,7 +24,7 @@ function BottomSheet({
         <Drawer.Overlay className="fixed inset-0 bg-black/25" />
         <Drawer.Content
           onPointerDownOutside={(event) => event.preventDefault()}
-          className="fixed right-2 bottom-8.5 left-2 pb-3 flex max-h-[90vh] flex-col overflow-hidden rounded-4xl bg-white shadow-[0_16px_60px_0_rgba(0,0,0,0.12),0_12px_20px_0_rgba(0,0,0,0.08),0_2px_8px_0_rgba(0,0,0,0.12)]"
+          className="fixed inset-x-2 bottom-8.5 pb-3 flex max-h-[90vh] flex-col overflow-hidden rounded-4xl bg-white shadow-[0_16px_60px_0_rgba(0,0,0,0.12),0_12px_20px_0_rgba(0,0,0,0.08),0_2px_8px_0_rgba(0,0,0,0.12)] sm:inset-x-0 sm:mx-auto sm:w-86"
         >
           <div className="mx-auto mt-3 h-1 w-14 shrink-0 cursor-grab rounded-[99px] bg-grey-100 active:cursor-grabbing" />
           <div className="flex flex-col gap-1.5 border-b border-grey-50 p-4">
