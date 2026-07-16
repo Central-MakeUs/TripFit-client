58ffe4c203ff12590d0ed3738b6ef09a06443199
nue-os <kimsoeun0108@gmail.com>
2026-07-16 14:02:29 +0900

    feat: 이전 날짜가 오늘 이전일 때 이전 날짜 화살표 비활성화

--- apps/web/components/schedule-day-bottom-sheet/ScheduleDayNavTitle.tsx
+++ apps/web/components/schedule-day-bottom-sheet/ScheduleDayNavTitle.tsx
@@ -8,12 +8,14 @@ type ScheduleDayNavTitleProps = {
   date: Date;
   onPrevDay: () => void;
   onNextDay: () => void;
+  isPrevDisabled?: boolean;
 };
 
 function ScheduleDayNavTitle({
   date,
   onPrevDay,
   onNextDay,
+  isPrevDisabled,
 }: ScheduleDayNavTitleProps) {
   return (
     <div className="flex w-full items-center justify-between">
@@ -21,6 +23,7 @@ function ScheduleDayNavTitle({
         size="small"
         icon={<ArrowLeftIcon className="text-grey-500" />}
         onClick={onPrevDay}
+        disabled={isPrevDisabled}
         aria-label="이전 날짜"
       />
       <span className="text-body-03">{format(date, 'yyyy년 M월 d일')}</span>
